549bb24d03986db975cc9044316f5a96ad780c9c
Candice Smith MacKenzie <Mahony8@aol.com>
2021-11-06 16:32:21 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(cb){
+    return cb();
+}
+receivesAFunction(function () {return Spy;});
+
+function returnsANamedFunction() {
+  return function named(){}
+}
+
+ 
+
+function returnsAnAnonymousFunction() {
+    return function(){
+    }
+}
